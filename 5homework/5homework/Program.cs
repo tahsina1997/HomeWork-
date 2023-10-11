b3a64bb01af5d9a1174d434ccd5d1b8c8d1b2228
tahsina1997 <146942859+tahsina1997@users.noreply.github.com>
2023-10-11 14:29:25 +0500

    Add files via upload

---
+++ 5homework/5homework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 9; i <= 0; i++) ;
+            Console.WriteLine();//
+
+            //2 domashka
+            Console.WriteLine("Write price of foods");
+            int price = int.Parse(Console.ReadLine());
+            for (int i=1; i<=10; i++)
+                int price= i*price;
+            Console.WriteLine(price )// nopurra
+        }
+    }
+}
