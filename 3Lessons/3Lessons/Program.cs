ba4a750085261666981ffe994da1ba94df327db8
tahsina1997 <146942859+tahsina1997@users.noreply.github.com>
2023-10-11 14:23:20 +0500

    Add files via upload

---
+++ 3Lessons/3Lessons/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse (Console.ReadLine());
+            if ( n > 0) n++;
+            else n -=2;
+            Console.WriteLine(n);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
