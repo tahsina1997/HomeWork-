fe531637d8cdd372a4e4a88ee7e61f81fd23a49a
tahsina1997 <146942859+tahsina1997@users.noreply.github.com>
2023-10-11 14:01:43 +0500

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Anna, where are you?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Okey, I am coming");
+            string name = Console.ReadLine();
+        }
+    }
+}
