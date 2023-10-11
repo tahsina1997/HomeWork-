0ac972b7eb66345fa51926e66729ef6d655950b6
tahsina1997 <146942859+tahsina1997@users.noreply.github.com>
2023-10-11 14:18:51 +0500

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assalom, in kurtachai dukhtaronaaton chand pul ast?");
+            string libos1 = Console.ReadLine();
+            Console.WriteLine("Ba kudaki dusola razmer dihed");
+            string libos2 = Console.ReadLine();
+            Console.WriteLine("Pulashro ba kassa diham");
+            string libos3 = Console.ReadLine();
+            Console.WriteLine("rahmat");
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+
